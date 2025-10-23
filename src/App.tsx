9ea3c286c3821a46517582c606979f9a2df7767b
Leonardo Mendes da Silva <118060562+oleonardomendes@users.noreply.github.com>
2025-10-23 08:47:13 -0300

    Fix import paths for VehicleDetails and Vehicles

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
-import VehicleDetails from "./pages/VehicleDetails";
-import Vehicles from "./pages/Vehicles";
+import VehicleDetails from "./pages/vehicleDetails";
+import Vehicles from "./pages/vehicles";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
